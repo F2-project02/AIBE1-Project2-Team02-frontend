5c548df6facac31928b2ec8ca0a91c3823f8b094
1000hyehyang <ducogus12@gmail.com>
2025-05-03 21:35:46 +0900

    feat : 검색창 UI만 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 // src/pages/Home.jsx
 
-import { Box, Typography } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import HeroBanner from "../components/Home/HeroBanner/HeroBanner";
+import SearchBar from "../components/Home/SearchBar";
 
 export default function Home() {
   return (
     <Box sx={{ mt: 4 }}>
-      <HeroBanner />
+      <Stack spacing={{ xs: 3, sm: 4, md: 5 }} alignItems="center">
+        <HeroBanner />
+        <SearchBar />
+      </Stack>
     </Box>
   );
 }
