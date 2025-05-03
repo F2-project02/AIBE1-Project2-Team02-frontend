bf6f0c2a711978044f068b016c771cd982866b9a
1000hyehyang <ducogus12@gmail.com>
2025-05-03 21:02:30 +0900

    refactor: App, main, Router 구조 정리 및 라우터 컨텍스트 위치 수정
    - main.jsx: Router 컨텍스트 보장을 위해 App 내부에서 BrowserRouter 제거
    - App.jsx: Router, ThemeProvider, CssBaseline 포함한 앱 진입점 역할로 변경
    - Router.jsx: 실제 라우팅 정의만 담당하도록 분리

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,23 +1,12 @@
-// src/router/Router.jsx
+// src/router/Router.jsx – Routes 전용 구성
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import Layout from "../components/layout/Layout";
-import Navbar from "../components/layout/Navbar";
-import LoginModal from "../components/auth/LoginModal";
-import Footer from "../components/layout/Footer";
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
 
 export default function AppRouter() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Layout>
-      <LoginModal /> {/* 항상 렌더링되지만 상태로만 열고 닫힘 */}
-      <Footer />
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
