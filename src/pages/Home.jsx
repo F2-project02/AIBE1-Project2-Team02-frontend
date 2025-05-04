de26a3ae1b787a1a59b1955b9782b699c0e3ab93
1000hyehyang <ducogus12@gmail.com>
2025-05-05 00:52:29 +0900

    feat(review): 후기 롤링 섹션 구현 및 자동 무한 스크롤 애니메이션 추가
    - ReviewSection 컴포넌트 추가: 후기 카드 자동 롤링 UI 구현 (marquee 방식)
    - ReviewCard 리팩토링: Figma 디자인 기준 맞춤 스타일 적용
    - 후기 카드 skeleton 컴포넌트 추가 (ReviewCardSkeleton)
    - hover 시 애니메이션 일시 정지 기능 적용
    - 후기 본문 ellipsis 2줄 처리 및 반응형 고려

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,18 +4,23 @@ import { Box, Stack } from "@mui/material";
 import HeroBanner from "../components/Home/HeroBanner/HeroBanner";
 import SearchBar from "../components/Home/SearchBar";
 import PopularMentorList from "../components/Home/PopularMentorList/PopularMentorList";
-import TestCors from "../components/TestCors";
 import CourseSection from "../components/CourseSection/CourseSection";
+import ReviewSection from "../components/Home/ReviewSection/ReviewSection";
 
 export default function Home() {
   return (
-    <Box sx={{ mt: 4 }}>
-      <Stack spacing={{ xs: 3, sm: 4, md: 5 }} alignItems="center">
+    <Box sx={{ mt: { xs: 4, md: 8 }, mb: { xs: 6, md: 10 } }}>
+      {/* 상단 배너 + 검색바 */}
+      <Stack spacing={{ xs: 4, sm: 5, md: 6 }} alignItems="center">
         <HeroBanner />
         <SearchBar />
+      </Stack>
+
+      {/* 메인 콘텐츠 영역 */}
+      <Stack spacing={{ xs: 8, sm: 10, md: 12 }} sx={{ mt: { xs: 10, sm: 12, md: 16 } }}>
         <PopularMentorList />
         <CourseSection />
-        <TestCors />
+        <ReviewSection />
       </Stack>
     </Box>
   );
