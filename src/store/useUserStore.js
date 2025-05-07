e2aa97a6eea86e7fd6f4486e35af0ef11c1b1383
1000hyehyang <ducogus12@gmail.com>
2025-05-07 15:53:34 +0900

    fix : useUserStore 기본값 해제

--- src/store/useUserStore.js
+++ src/store/useUserStore.js
@@ -2,13 +2,12 @@
 import { create } from "zustand";
 
 export const useUserStore = create((set) => ({
-  // 테스트용 강제 로그인된 멘토 상태
-  isLoggedIn: true,
-  userId: 201,
-  nickname: "가나다",
+  isLoggedIn: false,
+  userId: null,
+  nickname: "",
   profileImage: "/images/default-profile.svg",
-  role: "MENTEE", // MENTOR
-  myLectureIds: [1], // 현재 열람 중인 강의 ID 포함
+  role: "MENTEE", // 기본값
+  myLectureIds: [],
 
   // 로그인 처리
   login: ({ userId, nickname, profileImage, role, myLectureIds }) =>
@@ -22,7 +21,8 @@ export const useUserStore = create((set) => ({
     }),
 
   // 로그아웃 처리
-  logout: () =>
+  logout: () => {
+    localStorage.removeItem("token");
     set({
       isLoggedIn: false,
       userId: null,
@@ -30,5 +30,6 @@ export const useUserStore = create((set) => ({
       profileImage: "/images/default-profile.svg",
       role: "MENTEE",
       myLectureIds: [],
-    }),
+    });
+  },
 }));
